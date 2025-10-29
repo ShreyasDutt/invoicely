48a12c206fffd635917e45fc949f35df5cdfe564
Shreyas Dutt <151562739+ShreyasDutt@users.noreply.github.com>
2025-10-28 19:06:41 -0700

    Nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Nav from '@/components/shared/Nav'
 
 const page = () => {
   return (
-    <div>
+    <div className='max-w-5xl mx-auto px-4 py-4'>
       <Nav/>
     </div>
   )
--- components/shared/Nav.tsx
+++ components/shared/Nav.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import { Button } from '../ui/button'
 import { ModeToggle } from './ThemeSwitch'
 
 const Nav = () => {
   return (
-    <div className='flex items-center justify-between max-w-5xl mx-auto px-4 py-4'>
+    <div className='flex items-center justify-between border-2 border-dashed p-2'>
         <div>
             <p className='font-mono text-2xl'>Invoicely</p>
         </div>
